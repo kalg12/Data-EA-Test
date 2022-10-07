d8bf42f87283c11ca545d7a651577fe9c13aae73
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-10-07 00:33:04 -0500

    Creación de ruta

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {BrowserRouter as Router} from 'react-router-dom'
+
+const AppRoutes = () => {
+  return (
+    <Router>
+
+    </Router>
+  )
+}
+
+export default AppRoutes
