4457dedfe782deda4a025b3186c1401b3a8abd6f
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-10-14 21:49:32 -0500

    Answers destructured

--- src/components/TableIndividual.jsx
+++ src/components/TableIndividual.jsx
@@ -48,6 +48,52 @@ const TableIndividual = () => {
     }
   };
 
+  console.log(answers[0]);
+
+  /* Destrcturamos answers.[0], los 40 elementos, element1, element2... hasta element40 */
+  const {
+    element1,
+    element2,
+    element3,
+    element4,
+    element5,
+    element6,
+    element7,
+    element8,
+    element9,
+    element10,
+    element11,
+    element12,
+    element13,
+    element14,
+    element15,
+    element16,
+    element17,
+    element18,
+    element19,
+    element20,
+    element21,
+    element22,
+    element23,
+    element24,
+    element25,
+    element26,
+    element27,
+    element28,
+    element29,
+    element30,
+    element31,
+    element32,
+    element33,
+    element34,
+    element35,
+    element36,
+    element37,
+    element38,
+    element39,
+    element40,
+  } = answers[0] || {};
+
   return (
     <>
       <h1>Tabla individual por alumno</h1>
@@ -77,8 +123,19 @@ const TableIndividual = () => {
 		</thead>
 		<tbody>
 			<td>1</td>
-      {/* <td>{console.log(answers[0].element1)}</td> */}
+      {/* Si el elemento es A, lo pintamos en el primer campo, si es B, en el segundo y si es C en el tercero */}
+      <td>{element1 === "A" ? "X" : ""}</td>
+      <td>{element1 === "B" ? "X" : ""}</td>
+      <td>{element1 === "C" ? "X" : ""}</td>
 		</tbody>
+    <tbody>
+			<td>2</td>
+      {/* Si el elemento es A, lo pintamos en el primer campo, si es B, en el segundo y si es C en el tercero */}
+      <td>{element2 === "A" ? "X" : ""}</td>
+      <td>{element2 === "B" ? "X" : ""}</td>
+      <td>{element2 === "C" ? "X" : ""}</td>
+		</tbody>
+
 	</table>
       <hr></hr>
       <table>
